c059c9d7047e22bdcaad5ca9dfbec19a2fa23559
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2024-11-16 08:15:56 +0100

    Adjust rendering

---
+++ smartapp/src/main/java/edu/stevenfil/smart/smartapp/frontend/DynamicStringValue.java
@@ -0,0 +1,9 @@
+package edu.stevenfil.smart.smartapp.frontend;/**
+ * <b><class short description - 1 Line!></b>
+ * 
+ * <p><More detailed description - When to use, what it solves, etc.></p>
+ *
+ * @since <version tag>
+ */
+public class DynamicStringValue {
+}
